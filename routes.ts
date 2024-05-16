ef9fc6fdaf3336ee71a8547fa01492bbacd24401
Gustavo Tavares <gt3oliveira@gmail.com>
2024-05-16 17:46:34 -0300

    feat: add credentials user and authentications to routes public or private

---
+++ routes.ts
@@ -0,0 +1,30 @@
+/**
+ * Array de rotas públicas
+ * Rotas que não precisam de autenticação
+ * @type {string[]}
+ */
+export const publicRoutes = [
+  "/"
+]
+
+/**
+ * Array de rotas autenticadas
+ * Rotas que direcionam o usuário logado para /settings
+ * @type {string[]}
+ */
+export const authRoutes = [
+  "/auth/login",
+  "/auth/register",
+]
+
+/**
+ * prefix API de rotas autenticadas
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * Redirecionamento padrão de path logado
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
