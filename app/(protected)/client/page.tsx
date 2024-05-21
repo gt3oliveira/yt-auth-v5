c18c8fac20d8ed59a84050938582e170afa104be
Gustavo Tavares <gt3oliveira@gmail.com>
2024-05-21 14:53:00 -0300

    feat: add server component

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import UserInfo from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+export default function ClientPage() {
+  const user = useCurrentUser();
+
+  return (
+    <div>
+      <UserInfo
+        user={user}
+        label="📱 Client component"
+      />
+    </div>
+  )
+}
--- auth.ts
+++ auth.ts
@@ -51,6 +51,10 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (token.role && session.user) {
         session.user.role = token.role
       }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled
+      }
       return session
     },
     async jwt({ token }) {
@@ -60,6 +64,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (!existingUser) return token;
 
       token.role = existingUser.role
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
 
       return token
     },
